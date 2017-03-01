338f1c35b5bfc4246f0db9eec18fdb11f1840fab
Patrick Baxter <pb@coreos.com>
2017-03-01 21:17:26 +0000

    hyperkube v1.6.0-beta.0

--- pkg/asset/internal/templates.go
+++ pkg/asset/internal/templates.go
@@ -35,7 +35,7 @@ spec:
     spec:
       containers:
       - name: kubelet
-        image: quay.io/coreos/hyperkube:v1.5.3_coreos.0
+        image: quay.io/coreos/hyperkube:v1.6.0-beta.0_coreos.0
         command:
         - ./hyperkube
         - kubelet
@@ -132,7 +132,7 @@ spec:
       hostNetwork: true
       containers:
       - name: kube-apiserver
-        image: quay.io/coreos/hyperkube:v1.5.3_coreos.0
+        image: quay.io/coreos/hyperkube:v1.6.0-beta.0_coreos.0
         command:
         - /usr/bin/flock
         - --exclusive
@@ -229,7 +229,7 @@ spec:
         master: "true"
       containers:
       - name: kube-controller-manager
-        image: quay.io/coreos/hyperkube:v1.5.3_coreos.0
+        image: quay.io/coreos/hyperkube:v1.6.0-beta.0_coreos.0
         command:
         - ./hyperkube
         - controller-manager
@@ -286,7 +286,7 @@ spec:
         master: "true"
       containers:
       - name: kube-scheduler
-        image: quay.io/coreos/hyperkube:v1.5.3_coreos.0
+        image: quay.io/coreos/hyperkube:v1.6.0-beta.0_coreos.0
         command:
         - ./hyperkube
         - scheduler
@@ -319,7 +319,7 @@ spec:
       hostNetwork: true
       containers:
       - name: kube-proxy
-        image: quay.io/coreos/hyperkube:v1.5.3_coreos.0
+        image: quay.io/coreos/hyperkube:v1.6.0-beta.0_coreos.0
         command:
         - /hyperkube
         - proxy
